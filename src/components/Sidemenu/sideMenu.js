62b1c78a7c35d5d732b6f1ae801382d238b1b2e7
Oussama_Aissouni <oussamaaissouni@gmail.com>
2020-12-25 22:27:43 +0100

    Commit test,Added framworks files

---
+++ src/components/Sidemenu/sideMenu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./sideMenu.css";
+
+export default function sideMenu() {
+    return (
+        <div>
+            <div className="sidemenu">
+                
+            </div>
+        </div>
+    )
+}
